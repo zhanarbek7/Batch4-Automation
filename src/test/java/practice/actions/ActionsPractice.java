62e6f47eb8fdfaf720d2d34a002e7e991e16e706
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2023-12-09 09:13:57 -0600

    Commit

--- src/test/java/practice/actions/ActionsPractice.java
+++ src/test/java/practice/actions/ActionsPractice.java
@@ -8,158 +8,156 @@ import java.time.Duration;
 
 public class ActionsPractice {
     public static void main(String[] args) throws InterruptedException {
-        actionsMoveByOffset();
+        actionsKeysExercise();
     }
-
 
     public static void scrollToElement(){
         WebDriver driver = Driver.getDriver();
-        driver.get("https://en.wikipedia.org/wiki/Java_(programming_language)");
+        driver.get("https://en.wikipedia.org/wiki/Java_(programming_language)#cite_note-40");
+
         WebElement table = driver.findElement(By.xpath("//table[@class='wikitable']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", table);
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView(true)", table);
     }
 
     public static void actionsMoveToElement(){
         WebDriver driver = Driver.getDriver();
-        driver.get("https://en.wikipedia.org/wiki/Java_(programming_language)");
+        driver.get("https://en.wikipedia.org/wiki/Java_(programming_language)#cite_note-40");
+
         WebElement table = driver.findElement(By.xpath("//table[@class='wikitable']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", table);
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView(true)", table);
+
         Actions actions = new Actions(driver);
-        WebElement betaLink = driver.findElement(By.linkText("Beta"));
-        actions.moveToElement(betaLink).perform();
+        WebElement betaLink = driver.findElement(By.partialLinkText("Beta"));
+
+        actions.moveToElement(betaLink).moveToElement(betaLink).release().perform();
     }
 
     public static void actionsScrollByAmount(){
-//        You are using the scrollByAmount method of the Actions class to scroll the web page vertically.
-//        The method takes two parameters: x (horizontal scroll) and y (vertical scroll).
-//        In this case, you are scrolling vertically by 3000 pixels (0 for horizontal scroll, and 3000 for vertical scroll).
         WebDriver driver = Driver.getDriver();
-        driver.get("https://en.wikipedia.org/wiki/Java_(programming_language)");
+        driver.get("https://www.amazon.com/");
+
         Actions actions = new Actions(driver);
-        actions.scrollByAmount(0,3000).perform();
+        // x=0,y=0
+        actions.scrollByAmount(0,10000).perform();
     }
 
-    public static void actionsClickAndHold(){
-        // In this example, first we move cursor to the location of title C
-        // then click and hold title C
-        // then when we move mouse it moves title C also
+    public static void actionsClickAndHold() throws InterruptedException {
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
+        driver.get("https://selenium08.blogspot.com/2020/01/click-and-hold.html");
 
-        String url = "https://selenium08.blogspot.com/2020/01/click-and-hold.html";
-        driver.get(url);
-
-        // Locate the element C by By.xpath.
-        WebElement titleC = driver.findElement(By.xpath("//li[text()= 'C']"));
-
-        // Create an object of actions class and pass reference of WebDriver as a parameter to its constructor.
         Actions actions = new Actions(driver);
-        // Move the cursor to the position of element C.
-        actions.moveToElement(titleC); // Call clickAndHold() method to perform click and hold operation.
-        actions.clickAndHold().perform();
+        // find the button C
+        WebElement buttonC = driver.findElement(By.xpath("//li[@name='C']"));
+        // click and hold button C for 10 seconds
+        actions.clickAndHold(buttonC).perform();
+        Thread.sleep(10000);
+        // after 10 seconds release button C
+        actions.release().perform();
     }
 
     public static void actionsClickAndHoldExercise(){
-        //
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
-        String url = "https://selenium08.blogspot.com/2020/01/click-and-hold.html";
-        driver.get(url);
-        // Locate the element C by By.xpath.
-        WebElement titleC = driver.findElement(By.xpath("//li[text()= 'C']"));
-        WebElement titleB = driver.findElement(By.xpath("//li[text()= 'B']"));
-        // Create an object of actions class and pass reference of WebDriver as a parameter to its constructor.
+        driver.get("https://selenium08.blogspot.com/2020/01/click-and-hold.html");
+        WebElement titleC = driver.findElement(By.xpath("//li[@name='C']"));
+        WebElement titleB = driver.findElement(By.xpath("//li[@name='B']"));
         Actions actions = new Actions(driver);
-        // Move the cursor to the position of element C.
-        actions.moveToElement(titleC); // Call clickAndHold() method to perform click and hold operation.
-        actions.clickAndHold().moveToElement(titleB).release().perform();
+        actions.clickAndHold(titleC).moveToElement(titleB).release().perform();
     }
 
     public static void actionsDoubleClick(){
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
         driver.get("https://demoqa.com/buttons");
-
+        WebElement doubleClickButton = driver.findElement(By.id("doubleClickBtn"));
         Actions actions = new Actions(driver);
-        WebElement heading = driver.findElement(By.id("doubleClickBtn"));
-        actions.doubleClick(heading).perform();
+        actions.doubleClick(doubleClickButton).perform();
+
+        String expectedText = "You have done a double click";
+        String actualText = driver.findElement(By.id("doubleClickMessage")).getText();
+        if(expectedText.equals(actualText)){
+            System.out.println("Success");
+        }
+        else{
+            System.out.println("Failed");
+        }
     }
 
     public static void actionsRightClick(){
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
         driver.get("https://demoqa.com/buttons");
-
+        WebElement rightClickBtn = driver.findElement(By.id("rightClickBtn"));
         Actions actions = new Actions(driver);
-        WebElement heading = driver.findElement(By.id("rightClickBtn"));
-        actions.doubleClick(heading).perform();
+        actions.contextClick(rightClickBtn).perform();
+
+        String expectedText = "You have done a right click";
+        String actualText = driver.findElement(By.id("rightClickMessage")).getText();
+        if(expectedText.equals(actualText)){
+            System.out.println("Success");
+        }
+        else{
+            System.out.println("Failed");
+        }
     }
 
     public static void actionsDragAndDrop(){
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
         driver.get("https://demoqa.com/droppable");
+
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        WebElement droppable = driver.findElement(By.id("droppable"));
+
         Actions actions = new Actions(driver);
-
-        WebElement dragElement = driver.findElement(By.id("draggable"));
-        WebElement dropTo = driver.findElement(By.id("droppable"));
-
-        actions.dragAndDrop(dragElement,dropTo).perform();
+        actions.dragAndDrop(draggable, droppable).perform();
     }
 
     public static void actionsDragAndDropBy(){
-        // copy heading and pase it in the search box
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
         driver.get("https://demoqa.com/dragabble");
+        // go to this website = https://demoqa.com/draggable
+        // move draggable element to the bottom of the webpage
+        WebElement dragBox = driver.findElement(By.id("dragBox"));
         Actions actions = new Actions(driver);
-        WebElement dragElement = driver.findElement(By.xpath("//div[@id='draggableExample-tabpane-simple']/div"));
-        actions.dragAndDropBy(dragElement, 0,300).perform();
+        actions.dragAndDropBy(dragBox, 150,300).perform();
     }
 
-    public static void actionsKeys() throws InterruptedException {
+    public static void actionsCopy(){
         WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://en.wikipedia.org/wiki/Karate");
+        driver.get("https://www.google.com/");
         Actions actions = new Actions(driver);
-        WebElement heading = driver.findElement(By.xpath("//h1[@id='firstHeading']/span"));
-        // copying heading
-        actions.doubleClick(heading).keyDown(Keys.COMMAND).sendKeys("c").keyUp(Keys.COMMAND).perform();
-        WebElement searchBox = driver.findElement(By.xpath("//form[@id='searchform']//input[@class='cdx-text-input__input']"));
-        // sending keys
-        Thread.sleep(1000);
-        actions.keyDown(Keys.COMMAND).sendKeys(searchBox, "v").keyUp(Keys.COMMAND).sendKeys(Keys.ENTER).perform();
-    }
-
-    public static void actionsCopyWholeTextOfPage() throws InterruptedException {
-        WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://en.wikipedia.org/wiki/Karate");
-        Actions actions = new Actions(driver);
-        Thread.sleep(1000);
-        actions.keyDown(Keys.COMMAND).sendKeys("a").sendKeys("c")
-                .keyUp(Keys.COMMAND)
+        WebElement textBox = driver.findElement(By.xpath("//textarea[@name='q']"));
+        actions
+                .keyDown(Keys.SHIFT)
+                .sendKeys(textBox,"codewise")
+                .keyUp(Keys.SHIFT)
+                .sendKeys(" academy" + Keys.ENTER)
+                .keyUp(Keys.SHIFT)
                 .perform();
     }
 
-    public static void actionsMoveByOffset(){
-        WebDriver driver = Driver.getDriver();
-        driver.manage().window().maximize();
 
-        driver.get("https://demoqa.com/dragabble");
+    public static void actionsKeysExercise(){
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://en.wikipedia.org/wiki/Karate");
+        WebElement heading = driver.findElement(By.xpath("//span[@class='mw-page-title-main']"));
+        WebElement searchBox = driver.findElement(By.xpath("//form[@id='searchform']//input[@class='cdx-text-input__input']"));
+
         Actions actions = new Actions(driver);
-        WebElement elementToHold = driver.findElement(By.id("dragBox"));
-        actions.clickAndHold(elementToHold).moveByOffset(100,200).perform();
+        actions.doubleClick(heading)
+                .keyDown(Keys.COMMAND)
+                .sendKeys("c")
+                .keyUp(Keys.COMMAND)
+                .keyDown(Keys.COMMAND)
+                .click(searchBox)
+                .sendKeys("v")
+                .keyUp(Keys.COMMAND)
+                .pause(Duration.ofMillis(200))
+                .sendKeys(" Combat")
+                .sendKeys(Keys.ENTER)
+                .perform();
+
     }
 
-    
+
 
 }
-
